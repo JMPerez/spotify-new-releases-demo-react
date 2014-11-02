13b567884ce75132f674f8e627eedc276dbb93e2
José M. Pérez <jmperez1985@gmail.com>
2014-11-02 13:15:37 +0100

    Fix redirect uri

--- js/oauth-config.js
+++ js/oauth-config.js
@@ -6,8 +6,7 @@ var OAuthConfig = (function() {
   "My Applications" section of the Spotify developer site */
 
   var clientId = 'b3e64e390a1449bfbbaf4075eb6bbab9';
-  //var redirectUri = 'http://jmperezperez.com/spotify-new-releases-demo/callback.html';
-  var redirectUri = 'http://localhost:8000/callback.html';
+  var redirectUri = 'http://jmperezperez.com/spotify-new-releases-demo-react/callback.html';
 
   var host = /http[s]?:\/\/[^/]+/.exec(redirectUri)[0];
 
