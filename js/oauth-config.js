d46dfbbd9942b402cb76e02643e3e76991f14a1c
José Manuel Pérez <jmperez1985@gmail.com>
2015-12-08 17:27:05 +0100

    http => https

--- js/oauth-config.js
+++ js/oauth-config.js
@@ -6,7 +6,7 @@ var OAuthConfig = (function() {
   "My Applications" section of the Spotify developer site */
 
   var clientId = 'b3e64e390a1449bfbbaf4075eb6bbab9';
-  var redirectUri = 'http://jmperezperez.com/spotify-new-releases-demo-react/callback.html';
+  var redirectUri = 'https://jmperezperez.com/spotify-new-releases-demo-react/callback.html';
 
   var host = /http[s]?:\/\/[^/]+/.exec(redirectUri)[0];
 
